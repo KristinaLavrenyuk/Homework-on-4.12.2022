ee69703666ec834f2169ba80b3e9fd1aa1c75bc9
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-12-02 18:37:03 +0300

    Add files via upload

---
+++ AgroSchoolBoy.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+
+struct agro{
+    char name;
+    int clas;
+    float ocenka;
+    int quanPropusk;
+    int power;
+};
+
+int main()
+{
+    agro schoolBoy;
+    cout << "Name: ";
+    cin >> schoolBoy.name;
+    cout << "Class: ";
+    cin >> schoolBoy.clas;
+    cout << "Grade: ";
+    cin >> schoolBoy.ocenka;
+    cout << "Quantity propuskov: ";
+    cin >> schoolBoy.quanPropusk;
+    cout << "Power: ";
+    cin >> schoolBoy.power;
+    cout << schoolBoy.name << endl << schoolBoy.clas << endl << schoolBoy.ocenka << endl << schoolBoy.quanPropusk << endl << schoolBoy.power;
+    system("pause");
+}
